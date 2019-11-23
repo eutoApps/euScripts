9ce8612a3d911cad5e0bad8cdbc5cd166696bb84
eutoApps <58101651+eutoApps@users.noreply.github.com>
2019-11-23 17:43:31 +0700

    Add files via upload

---
+++ fileSelection.py
@@ -0,0 +1,13 @@
+from tkinter.filedialog import askopenfilename
+
+'''
+This Python script gives the address of a file that the user clicks.
+
+by eutoApps / Eric Utomo
+November 2019
+'''
+
+address = askopenfilename()
+print(address)
+
+#Eric Utomo
